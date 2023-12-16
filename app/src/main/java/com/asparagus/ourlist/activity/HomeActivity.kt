997cd44949469b28715e18fe26f6acf4083d4929
unxpctdchrs <theunexpectedchris@gmail.com>
2023-12-16 23:25:35 +0700

    redid the home activity and deleting some stuff

--- app/src/main/java/com/asparagus/ourlist/activity/HomeActivity.kt
+++ app/src/main/java/com/asparagus/ourlist/activity/HomeActivity.kt
@@ -1,134 +1,51 @@
 package com.asparagus.ourlist.activity
 
-import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.EditText
-import android.widget.LinearLayout
 import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.asparagus.ourlist.R
-import com.asparagus.ourlist.adapter.ListAdapter
+import com.asparagus.ourlist.adapter.TaskAdapter
 import com.asparagus.ourlist.databinding.ActivityHomeBinding
-import com.asparagus.ourlist.model.List
-import com.google.firebase.Firebase
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.ValueEventListener
-import com.google.firebase.database.database
+import com.asparagus.ourlist.fragment.NewToDoListFragment
+import com.asparagus.ourlist.model.TaskViewModel
+import com.asparagus.ourlist.model.ToDoItem
 
-class HomeActivity : AppCompatActivity(), ListAdapter.ListAdapterInterface, ToDoActivity.OnDialogNextBtnClickListener {
+class HomeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHomeBinding
-    private lateinit var listDialog: Dialog
-
-    private lateinit var database: DatabaseReference
-    private lateinit var auth: FirebaseAuth
-    private lateinit var authId: String
-
-    private lateinit var listAdapter: ListAdapter
-    private lateinit var toDoItemList: ArrayList<List>
+    private lateinit var taskViewModel: TaskViewModel
+    private var items: ArrayList<ToDoItem> = arrayListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        auth = FirebaseAuth.getInstance()
-        authId = auth.currentUser!!.uid
-        database = Firebase.database.reference.child("Tasks").child(authId)
+        taskViewModel = ViewModelProvider(this).get(TaskViewModel::class.java)
 
-        binding.listRecyclerView.setHasFixedSize(true)
-        binding.listRecyclerView.layoutManager = LinearLayoutManager(this)
-
-        toDoItemList = arrayListOf()
-        listAdapter = ListAdapter(toDoItemList)
-        listAdapter.setListener(this)
-        binding.listRecyclerView.adapter = listAdapter
-
-        getTaskFromFirebase()
-
-        binding.addListBtn.setOnClickListener{
-            listDialog = Dialog(this@HomeActivity)
-            listDialog.setContentView(R.layout.activity_to_do)
-            listDialog.window!!.setLayout(
-                LinearLayout.LayoutParams.WRAP_CONTENT,
-                LinearLayout.LayoutParams.WRAP_CONTENT
-            )
-            listDialog.show()
+        binding.addNewTaskBtn.setOnClickListener{
+            NewToDoListFragment(null).show(supportFragmentManager, "newTodoList")
         }
-    }
 
-    private fun getTaskFromFirebase() {
-        database.addValueEventListener(object : ValueEventListener {
-            override fun onDataChange(snapshot: DataSnapshot) {
-
-                toDoItemList.clear()
-                for (taskSnapshot in snapshot.children) {
-                    val todoTask = taskSnapshot.key?.let { List(it, taskSnapshot.value.toString()) }
-
-                    if (todoTask != null) {
-                        toDoItemList.add(todoTask)
-                    }
-
-                }
-                Log.d("HomeActivity", "onDataChange: " + toDoItemList)
-                listAdapter.notifyDataSetChanged()
-
-            }
-
-            override fun onCancelled(error: DatabaseError) {
-                Toast.makeText(this@HomeActivity, error.toString(), Toast.LENGTH_SHORT).show()
-            }
-
-
-        })
-    }
-
-    override fun onDeleteItemClicked(listData: List, position: Int) {
-        listData.listId?.let {
-            database.child(it).removeValue().addOnCompleteListener {
-                if (it.isSuccessful) {
-                    Toast.makeText(this, "Deleted Successfully", Toast.LENGTH_SHORT).show()
-                } else {
-                    Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
-                }
-            }
-        }
-    }
-
-    override fun onEditItemClicked(listData: List, position: Int) {
-        TODO("Not yet implemented")
-    }
-
-    override fun saveTask(toDoTask: String, toDoEdit:EditText){
-        database
-            .push().setValue(toDoTask)
-            .addOnCompleteListener {
-                if (it.isSuccessful) {
-                    Toast.makeText(this, "Task Added Successfully", Toast.LENGTH_SHORT).show()
-                    toDoEdit.text = null
-
-                } else {
-                    Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
-                }
-            }
-        listDialog.dismiss()
-    }
-
-    override fun updateTask(listData: List, toDoEdit: EditText) {
-//        val map = HashMap<String, Any>()
-//        map[listData.listId] = listData.task
-//        database.updateChildren(map).addOnCompleteListener {
-//            if (it.isSuccessful) {
-//                Toast.makeText(this, "Updated Successfully", Toast.LENGTH_SHORT).show()
-//            } else {
-//                Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+//        binding.taskRecyclerView.layoutManager = LinearLayoutManager(this)
+//        binding.taskRecyclerView.setHasFixedSize(true)
+//        binding.taskRecyclerView.adapter = TaskAdapter(items, object : TaskAdapter.OnClickListener{
+//            override fun onItemClick(position: Int) {
+//                NewToDoListFragment(null).show(supportFragmentManager, "newTodoList")
 //            }
-//            listDialog.dismiss()
-//        }
-    }
+//        })
 
+        taskViewModel.taskItems.observe(this){
+            binding.taskRecyclerView.apply {
+                layoutManager = LinearLayoutManager(applicationContext)
+                adapter = it?.let { it1 ->
+                    TaskAdapter(it1, object : TaskAdapter.OnClickListener{
+                        override fun onItemClick(position: Int) {
+                            Toast.makeText(this@HomeActivity, "test click", Toast.LENGTH_SHORT).show()
+                        }
+                    })
+                }
+            }
+        }
+    }
 }
